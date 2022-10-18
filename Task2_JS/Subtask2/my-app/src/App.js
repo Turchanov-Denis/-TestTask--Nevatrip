f1707c7b872b96f04b41dbb5433bf3b92f39735b
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-18 19:45:00 +0200

    output history orders

--- Task2_JS/Subtask2/my-app/src/App.js
+++ Task2_JS/Subtask2/my-app/src/App.js
@@ -1,11 +1,48 @@
 import ListItem from './component/ListItem';
 import './css/style.css'
+import { useState } from "react"
 
 
 
 function App() {
+  const [items, setItems] = useState()
+  const [historyOrders, setHistoryOrders] = useState({})
+  const [listHistoryOrders, setListHistoryOrders] = useState([])
+  function getList() {
+    // const listItems = document.querySelector(".list-item");
+    console.log(historyOrders);
+    (Number(historyOrders.amount) !== 0) && setListHistoryOrders(prev => [...prev, historyOrders])
+
+
+  }
+  function timeArrive(string) {
+    let date = new Date()
+    date.setHours(Number(string.slice(0, 2)))
+    date.setMinutes(Number(string.slice(3, 5)))
+    date.setTime(date.getTime() + 60 * 40 * 1000)
+    // date.setTime(Date.parse(string) + 60*40)
+
+
+    return (date.getHours() < 9 ? "0" + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 9 ? "0" + date.getMinutes() : date.getMinutes())
+  }
   return (
-    <ListItem></ListItem>
+    <div>
+      <section className='ticket'>
+        <ListItem historyOrders={historyOrders} setHistoryOrders={setHistoryOrders}></ListItem>
+        <button style={{
+          "width": "100px",
+          "height": "40px"
+        }} onClick={() => getList()}>Check out</button>
+      </section>
+
+      <section className='historyOrders'>
+        {listHistoryOrders.map(item => (item.route !== "из A в B и обратно в А") ? <div style={{
+          "marginTop": "30px"
+        }}>Вы выбрали {item.amount} билета по маршруту из {item.route} стоимостью {Number(item.amount) * 400}р. <br></br> Это путешествие займет у вас 40 минут. <br></br>Теплоход отправляется в {(item.route == "из A в B" ? item.timeTo : item.timeFrom)}, а прибудет в {timeArrive((item.route == "из A в B" ? item.timeTo : item.timeFrom))}</div> : <div style={{
+          "marginTop": "30px"
+        }}>Вы выбрали {item.amount} билета по маршруту из {item.route} стоимостью {Number(item.amount) * 400}р. <br></br>Теплоход отправляется в {item.timeTo}, а прибудет в {timeArrive(item.timeTo)}<br></br>Обратный путь: теплоход отправляется в {item.timeFrom}, а прибудет в {timeArrive(item.timeFrom)}</div>)}
+      </section>
+    </div>
   );
 }
 
--- Task2_JS/Subtask2/my-app/src/component/ListItem.js
+++ Task2_JS/Subtask2/my-app/src/component/ListItem.js
@@ -1,17 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
-export default function ListItem() {
+export default function ListItem({historyOrders,setHistoryOrders}) {
 
-    const [route, setRoute] = useState("")
-    const [timeTo,setTimeTo] = useState("")
-    const [timeFrom,setTimeFrom] = useState("")
-    const sheduleTo = ["18:00","18:30","18:45","19:00","19:15","21:00",]
-    const sheduleFrom = ["18:30","18:45","19:00","19:15","21:50","21:50"]
+    const [route, setRoute] = useState("из A в B")
+    const [timeTo, setTimeTo] = useState("18:00")
+    const [timeFrom, setTimeFrom] = useState("18:30")
+    const [amount, setAmount] = useState(0)
+    const sheduleTo = ["18:00", "18:30", "18:45", "19:00", "19:15", "21:00",]
+    const sheduleFrom = ["18:30", "18:45", "19:00", "19:15", "21:50", "21:50"]
 
-
+    useEffect(()=>{
+        setHistoryOrders({route,timeTo,timeFrom,amount})
+        console.log(historyOrders)
+    },[amount,route,timeTo,timeFrom])
     return (
-        <div>
+        <div className="list-item">
             <select name="route" id="route" onChange={e => setRoute(e.target.value)}>
                 <option value="из A в B">из A в B</option>
                 <option value="из B в A">из B в A</option>
@@ -19,37 +23,32 @@ export default function ListItem() {
             </select>
 
             <label for="time">Выберите время</label>
-            {(route === "из A в B") ? <select name="time" id="time">
-                <option value="18:00(из A в B)">18:00(из A в B)</option>
+            {(route === "из A в B") ? <select name="time" id="time" onChange={e => setTimeTo(e.target.value)}>
+                {sheduleTo.map(item => <option value={item}>{item}(из A в B)</option>)}
+                {/* <option value="18:00(из A в B)">18:00(из A в B)</option>
                 <option value="18:30(из A в B)">18:30(из A в B)</option>
                 <option value="18:45(из A в B)">18:45(из A в B)</option>
                 <option value="19:00(из A в B)">19:00(из A в B)</option>
                 <option value="19:15(из A в B)">19:15(из A в B)</option>
-                <option value="21:00(из A в B)">21:00(из A в B)</option>
-            </select> :<select name="time" id="time">
-            <option value="18:30(из B в A)">18:30(из B в A)</option>
+                <option value="21:00(из A в B)">21:00(из A в B)</option> */}
+            </select> : <select name="time" id="time" onChange={e => setTimeFrom(e.target.value)} >
+            {sheduleFrom.map(item => <option value={item}>{item}(из B в A)</option>)}
+                {/* <option value="18:30(из B в A)">18:30(из B в A)</option>
                 <option value="18:45(из B в A)">18:45(из B в A)</option>
                 <option value="19:00(из B в A)">19:00(из B в A)</option>
                 <option value="19:15(из B в A)">19:15(из B в A)</option>
                 <option value="19:35(из B в A)">19:35(из B в A)</option>
                 <option value="21:50(из B в A)">21:50(из B в A)</option>
-                <option value="21:55(из B в A)">21:55(из B в A)</option>
+                <option value="21:55(из B в A)">21:55(из B в A)</option> */}
             </select>}
-            
+
 
             {(route === "из A в B и обратно в А") && <><label for="time">Выберите обратное время</label>
-            <select name="time" id="time">
-            <option value="18:30(из B в A)">18:30(из B в A)</option>
-                <option value="18:45(из B в A)">18:45(из B в A)</option>
-                <option value="19:00(из B в A)">19:00(из B в A)</option>
-                <option value="19:15(из B в A)">19:15(из B в A)</option>
-                <option value="19:35(из B в A)">19:35(из B в A)</option>
-                <option value="21:50(из B в A)">21:50(из B в A)</option>
-                <option value="21:55(из B в A)">21:55(из B в A)</option>
-            </select>
-</> }
-            
-
+                <select name="time" id="time" onChange={e => setTimeTo(e.target.value)}>
+                {sheduleTo.map(item => <option value={item}>{item}(из A в B)</option>)}
+                </select>
+            </>}
+            {(route && (timeTo || timeFrom)) && <><label>amount</label> <input type="number" onChange={e => { setAmount((e.target.value)) }}></input></>}
         </div>
 
     )
